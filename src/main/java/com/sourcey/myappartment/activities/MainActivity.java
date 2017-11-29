600467a804636a99c491545eb245b855f05a5334
Dio4090 <diogo.rocha42@outlook.com>
2017-11-29 01:26:02 -0200

    Added home activity and select menu

--- src/main/java/com/sourcey/myappartment/activities/MainActivity.java
+++ src/main/java/com/sourcey/myappartment/activities/MainActivity.java
@@ -3,8 +3,6 @@ package com.sourcey.myappartment.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -39,40 +37,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tvEmail.setText(email);
         tvMobileNumber.setText(Integer.toString(mobile_number));
 
-//        Intent intent = new Intent(this, LoginActivity.class);
-//        startActivity(intent);
-
     }
 
     @Override
     public void onClick(View view) {
         switch(view.getId()) {
             case R.id.btn_logout:
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                startActivity(new Intent(MainActivity.this, ProjectsActivity.class));
                 break;
 
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
